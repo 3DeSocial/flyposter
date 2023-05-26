165aee69d13549187af476d5d4c61afd26de9889
Andrew van Duivenbode <andrew@creativetransmissions.com>
2023-05-26 17:12:30 +0100

    rm broken import

--- src/lib/workers/canvasWorker.js
+++ src/lib/workers/canvasWorker.js
@@ -1,6 +1,5 @@
 import * as THREE from 'https://unpkg.com/three@0.152.2/build/three.module.js';
 import { VRButton } from 'https://unpkg.com/three@0.152.2/examples/jsm/webxr/VRButton.js';
-import { Text } from 'troika-three-text';
 
 let renderer, roadSegment1, roadSegment2, tunnelTexture, movementSpeed,clientWidth,clientHeight;
 let camera,cameraGroup,scene, roadGroup = new THREE.Group(), cubes =[];
